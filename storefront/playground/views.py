3e8393ff1f811c3368e88821e77cc9c7db62ba6f
randailp <phanrandy45@gmail.com>
2022-10-01 22:07:27 -0500

    add hello endpoint to playground app

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# request -> response
+# request handler
+# action
+# not like android views (user don't see this)
+
+def say_hello(request):
+    return HttpResponse("Hello world!")
